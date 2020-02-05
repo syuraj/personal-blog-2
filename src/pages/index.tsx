ba8039b6871cfeab9c7d1b6cef986e515100c859
Suraj Shrestha <syuraj@gmail.com>
2020-02-04 22:18:56 -0500

    yarn format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,52 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import PersonalBlog from "../containers/HomePage"
-import SEO from "../components/seo"
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import PersonalBlog from '../containers/HomePage'
+import SEO from '../components/seo'
 
 const PersonalBlogPage = (props: any) => {
-  const { data } = props
+	const { data } = props
 
-  return (
-    <Layout>
-      <SEO
-        title="Personal Blog"
-        description={data.site.siteMetadata.description}
-      />
-      <PersonalBlog />
-    </Layout>
-  )
+	return (
+		<Layout>
+			<SEO title="Personal Blog" description={data.site.siteMetadata.description} />
+			<PersonalBlog />
+		</Layout>
+	)
 }
 
 export default PersonalBlogPage
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD [<span>] MMMM [</span>]")
-            title
-            description
-            tags
-            cover {
-              childImageSharp {
-                fluid(maxWidth: 1170, quality: 90) {
-                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+	query {
+		site {
+			siteMetadata {
+				title
+				description
+			}
+		}
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			edges {
+				node {
+					excerpt
+					fields {
+						slug
+					}
+					frontmatter {
+						date(formatString: "DD [<span>] MMMM [</span>]")
+						title
+						description
+						tags
+						cover {
+							childImageSharp {
+								fluid(maxWidth: 1170, quality: 90) {
+									...GatsbyImageSharpFluid_withWebp_tracedSVG
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 `
