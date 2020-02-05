9fd1bcbda80ca72d0fa480fd9ea82a1c92c23700
Suraj Shrestha <syuraj@gmail.com>
2020-02-04 22:25:13 -0500

    minor: name change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,147 +1,147 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+	path: `.env.${process.env.NODE_ENV}`
 })
 
 module.exports = {
-  siteMetadata: {
-    title: `Suraj`,
-    author: `Suraj`,
-    about: `A Tech Enthusiast. Exploring new ways to implement tech on daily lives. Passionate on JavaScript, .Net, Java, Python, Machine Learning.`,
-    description: `A Personal Blog`,
-    siteUrl: `https://https://surajshrestha.netlify.com`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        minify: false, // Breaks styles if not set to false
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/components`,
-        name: `components`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-              linkImagesToOriginal: true,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-katex`,
-            options: {
-              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-mermaid`,
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-          },
+	siteMetadata: {
+		title: `Suraj`,
+		author: `Suraj`,
+		about: `A Tech Enthusiast. Exploring new ways to implement tech on daily lives. Passionate on JavaScript, .Net, Java, Python, Machine Learning.`,
+		description: `A Personal Blog`,
+		siteUrl: `https://https://surajshrestha.netlify.com`
+	},
+	plugins: [
+		{
+			resolve: `gatsby-plugin-styled-components`,
+			options: {
+				minify: false // Breaks styles if not set to false
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/components`,
+				name: `components`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/blog`,
+				name: `blog`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/assets`,
+				name: `assets`
+			}
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+							linkImagesToOriginal: true
+						}
+					},
+					{
+						resolve: `gatsby-remark-responsive-iframe`,
+						options: {
+							wrapperStyle: `margin-bottom: 1.0725rem`
+						}
+					},
+					{
+						resolve: `gatsby-remark-katex`,
+						options: {
+							// Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+							strict: `ignore`
+						}
+					},
+					{
+						resolve: `gatsby-remark-mermaid`
+					},
+					{
+						resolve: `gatsby-remark-prismjs`
+					},
+					{
+						resolve: `gatsby-remark-prismjs`
+					},
 
-          {
-            resolve: `gatsby-remark-copy-linked-files`,
-          },
-          {
-            resolve: `gatsby-remark-smartypants`,
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-transformer-sharp`,
-    },
-    {
-      resolve: `gatsby-plugin-sharp`,
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-feed`,
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `StoryHub - Personal Blog Minimal`,
-        short_name: `StoryHub`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/favicon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-offline`,
-    },
-    {
-      resolve: `gatsby-plugin-react-helmet`,
-    },
-    {
-      resolve: `gatsby-plugin-typescript`,
-    },
-    {
-      resolve: `gatsby-plugin-lodash`,
-    },
-    {
-      resolve: `gatsby-source-instagram`,
-      options: {
-        // username: 'syuraj',
-        // access_token: ,
-        // instagram_id: ,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Poppins`,
-            variants: [`300`, `400`, `500`, `600`, `700`],
-          },
-          {
-            family: `Fira Sans`,
-            variants: [`100`, `300`, `400`, `500`, `600`, `700`],
-          },
-        ],
-      },
-    },
-  ],
+					{
+						resolve: `gatsby-remark-copy-linked-files`
+					},
+					{
+						resolve: `gatsby-remark-smartypants`
+					}
+				]
+			}
+		},
+		{
+			resolve: `gatsby-transformer-sharp`
+		},
+		{
+			resolve: `gatsby-plugin-sharp`
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				//trackingId: `ADD YOUR TRACKING ID HERE`,
+			}
+		},
+		{
+			resolve: `gatsby-plugin-feed`
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Suraj Shrestha - Personal Blog`,
+				short_name: `Suraj`,
+				start_url: `/`,
+				background_color: `#ffffff`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `content/assets/favicon.png`
+			}
+		},
+		{
+			resolve: `gatsby-plugin-offline`
+		},
+		{
+			resolve: `gatsby-plugin-react-helmet`
+		},
+		{
+			resolve: `gatsby-plugin-typescript`
+		},
+		{
+			resolve: `gatsby-plugin-lodash`
+		},
+		{
+			resolve: `gatsby-source-instagram`,
+			options: {
+				// username: 'syuraj',
+				// access_token: ,
+				// instagram_id: ,
+			}
+		},
+		{
+			resolve: `gatsby-plugin-prefetch-google-fonts`,
+			options: {
+				fonts: [
+					{
+						family: `Poppins`,
+						variants: [`300`, `400`, `500`, `600`, `700`]
+					},
+					{
+						family: `Fira Sans`,
+						variants: [`100`, `300`, `400`, `500`, `600`, `700`]
+					}
+				]
+			}
+		}
+	]
 }
